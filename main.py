addfc00d6f501feca8e398aaf9e023b65f1bbe9a
Mitja Pak <pmitja1@gmail.com>
2021-01-07 18:03:30 +0100

    fixing

--- main.py
+++ main.py
@@ -19,10 +19,7 @@ def index():
     session_token = request.cookies.get("session_token")
     user = db.query(User).filter_by(session_token=session_token).first()
 
-    if user:
-        return redirect(url_for("profile"))
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/register")
